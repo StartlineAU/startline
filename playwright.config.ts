2a503b95f2403ddc764b19fd4e868c07fcd12287
Lachlan Martin <lachlan.martin64@gmail.com>
2026-07-07 16:36:02 +1000

    chore: remove playright retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,12 +6,11 @@ export default defineConfig({
   expect: { timeout: 15000 },
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 2 : 4,
   reporter: "list",
   use: {
     baseURL: "http://localhost:3000",
-    trace: "on-first-retry",
   },
   projects: [
     {
