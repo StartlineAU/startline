943dcaeeb9cf3b9ee2f249382d9813a5a6a2dd0e
Lachlan Martin <lachlan.martin64@gmail.com>
2026-07-18 15:43:23 +1000

    Move about page test into homepage.spec.ts

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -2,6 +2,14 @@ import { test, expect } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
 import { goToHomepage } from "./helpers";
 
+test.describe("about page", () => {
+  test("about page visual snapshot", async ({ page }) => {
+    await page.goto("/about");
+    await page.waitForLoadState("networkidle");
+    await argosScreenshot(page, "about-page");
+  });
+});
+
 test.describe("homepage", () => {
   test("renders with expected title", async ({ page }) => {
     await goToHomepage(page);
