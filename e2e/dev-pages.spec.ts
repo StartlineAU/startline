eab52e073d6b3e9e6b052a132a7320e3c3ad8d4b
Lachlan Martin <lachlan.martin64@gmail.com>
2026-07-18 15:42:11 +1000

    Remove email preview route and test (overkill)

--- e2e/dev-pages.spec.ts
+++ e2e/dev-pages.spec.ts
@@ -1,19 +1,10 @@
 import { test } from "@playwright/test";
 import { argosScreenshot } from "@argos-ci/playwright";
-import { goToHomepage } from "./helpers";
 
 test.describe("about page", () => {
   test("about page visual snapshot", async ({ page }) => {
     await page.goto("/about");
     await page.waitForLoadState("networkidle");
     await argosScreenshot(page, "about-page");
-  });
-});
-
-test.describe("email previews", () => {
-  test("registration confirmation email visual snapshot", async ({ page }) => {
-    await page.goto("/dev/email-previews");
-    await page.waitForLoadState("networkidle");
-    await argosScreenshot(page, "email-registration-confirmation");
   });
 });
